4ebe480b597905a6fe90da41d44c9db84679d677
dragosbus <dragosbusuioc94@gmail.com>
2018-08-24 18:26:46 +0200

    minor change of style

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-
 const CartItem = props => (
-    <li>
-        <p>{props.name}</p>
-        <p>{props.counter}</p>
-        <button onClick={props.returnOneItem}>Return one item</button>
-        <button onClick={props.returnAllItems}>Return all items</button>
-    </li>
+  <li className="cart-item">
+    <p>{props.name}</p>
+    <p>-{props.counter}</p>
+    <div>
+      <button className="btn" onClick={props.returnOneItem}>
+        Return one item
+      </button>
+      <button className="btn" onClick={props.returnAllItems}>
+        Return all items
+      </button>
+    </div>
+  </li>
 );
 
 export default CartItem;
